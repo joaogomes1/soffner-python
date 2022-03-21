58d341cc4861d7c7e741d5829992efa751924a7c
joaogomes1 <joao.gomes45@fatec.sp.gov.br>
2022-03-21 20:29:30 -0300

    exercise 01

---
+++ 01.py
@@ -0,0 +1,9 @@
+userInput = input("Informe um número: ")
+
+# print("Número informado: " + userInput )
+
+if int(userInput) == 0:
+    print("O número informado é zero.")
+else:
+    print("O número informado não é zero.")
+    
