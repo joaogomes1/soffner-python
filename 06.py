8fc69b11c6bf7127c19caeb7078b275932892859
joaogomes1 <joao.gomes45@fatec.sp.gov.br>
2022-06-18 02:03:46 -0300

    adds exercise 06

---
+++ 06.py
@@ -0,0 +1,7 @@
+# 6. Escreva um programa que converta uma temperatura em Fahrenheit para Celsius.
+
+fahrenheit = float(input("Informe a temperatura em Fahrenheit: "))
+
+celsius = ( fahrenheit - 32 ) / 1.8
+
+print( "O valor em Celsius é: " + str(celsius) )
